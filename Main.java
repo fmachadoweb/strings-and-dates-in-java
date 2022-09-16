df0e85dc541880338869a0bb65d3d6e8b312dcd2
Fernando Machado <fernandomachadoweb@gmail.com>
2022-09-16 10:17:33 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+
+import java.time.LocalDateTime;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+        
+		String nome = "Fernando";
+
+        // ISO 8601
+        LocalDate hoje = LocalDate.now();
+        Locale brasil = new Locale("pt", "BR");
+        System.out.println();
+        String diaSemana = hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil);
+        String saudacao;
+        LocalDateTime agora = LocalDateTime.now();
+        if (agora.getHour() >= 0 && agora.getHour() < 12) {
+            saudacao = "Bom dia!";
+        } else if (agora.getHour() >= 12 && agora.getHour() < 18){
+            saudacao = "Boa tarde!";
+        } else if (agora.getHour() >= 18 && agora.getHour() < 24){
+            saudacao = "Boa Noite!";
+        } else {
+            saudacao = "Olá.";
+        }
+
+        System.out.printf("Olá, %s. Hoje é %s, %s.%n", nome, diaSemana, saudacao.toUpperCase());
+    }
+}
